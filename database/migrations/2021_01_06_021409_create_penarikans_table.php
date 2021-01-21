3a0b426213295419b973f9e80a4d49cf42d6f555
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-01-21 09:47:00 +0700

    dauruang

--- database/migrations/2021_01_06_021409_create_penarikans_table.php
+++ database/migrations/2021_01_06_021409_create_penarikans_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePenarikanTable extends Migration
+class CreatePenarikansTable extends Migration
 {
     /**
      * Run the migrations.
