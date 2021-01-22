665bea51e4e4b07557931993fdb365d4d6d6d698
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-01-22 14:04:40 +0700

    dauruang

--- resources/views/components/side-nav.blade.php
+++ resources/views/components/side-nav.blade.php
@@ -2,9 +2,9 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('home')}}">
-        <div class="sidebar-brand-icon rotate-n-15">
+        {{-- <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-fire"></i>
-        </div>
+        </div> --}}
         <div class="sidebar-brand-text mx-3">DaurUang</div>
     </a>
 
