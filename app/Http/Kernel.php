76b9029a38f17316e6c0a89cff20e8642981c147
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-01-22 09:42:28 +0700

    dauruang

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\Cors::class,
     ];
 
     /**
@@ -64,8 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
-
         'user.web' => \App\Http\Middleware\UserWeb::class,
         'admin' => \App\Http\Middleware\Admin::class,
     ];
