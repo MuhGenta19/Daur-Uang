9eb7eba3736a4f89c47673091a80e87c9a29edc6
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-01-22 09:17:06 +0700

    dauruang

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::fallback(function () {
     return view('errors.404');
 });
 
-Route::group(['namespace' => 'Web', 'middleware' => ['user.web']], function () {
+Route::group(['namespace' => 'Web', 'middleware' => ['userweb']], function () {
 
     // Route Dashboard      -> Admin, Bendahara
     Route::get('/home', 'HomeController@index')->name('home');  //Dashboard
